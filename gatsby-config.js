b34aa632708c155bcd9d54fa28af9fb7e9a98915
annika.kubaj <70285472+ak0212@users.noreply.github.com>
2021-11-26 14:43:54 +0100

    connected contentful and added querys

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,23 @@
+require ("dotenv").config({
+  path: `.env${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
     title: "labb1blogg",
   },
-  plugins: [],
+  plugins: [
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-image',
+  {
+    resolve: `gatsby-source-contentful`,
+    options: {
+      spaceId: `2hnrppdpi44w`,
+      accessToken: `PnCuVEH1gxm-C0Bq0y0RbpeVsI7u9AyYyhcaeI60FSc`,
+    
+    },
+   },
+  ],
 };
+
